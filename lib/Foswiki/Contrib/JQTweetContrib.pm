a158ffdba8f8a7ea044535d003fbf592890a549b
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/JQTweetContrib@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/JQTweetContrib.pm
+++ lib/Foswiki/Contrib/JQTweetContrib.pm
@@ -3,14 +3,15 @@ package Foswiki::Contrib::JQTweetContrib;
 use strict;
 use warnings;
 
-our $VERSION = '$Rev$';
-our $RELEASE = '1.0';
-our $SHORTDESCRIPTION = 'Put twitter on your website';
+our $VERSION           = '$Rev$';
+our $RELEASE           = '1.0';
+our $SHORTDESCRIPTION  = 'Put twitter on your website';
 our $NO_PREFS_IN_TOPIC = 1;
 
 sub init {
-  require Foswiki::Plugins::JQueryPlugin ();
-  Foswiki::Plugins::JQueryPlugin::registerPlugin("tweet", "Foswiki::Contrib::JQTweetContrib::Core");
+    require Foswiki::Plugins::JQueryPlugin();
+    Foswiki::Plugins::JQueryPlugin::registerPlugin( "tweet",
+        "Foswiki::Contrib::JQTweetContrib::Core" );
 }
 
 1;
--- lib/Foswiki/Contrib/JQTweetContrib/Core.pm
+++ lib/Foswiki/Contrib/JQTweetContrib/Core.pm
@@ -7,24 +7,24 @@ use Foswiki::Plugins::JQueryPlugin::Plugin ();
 our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
 
 sub new {
-  my $class = shift;
+    my $class = shift;
 
-  my $this = bless(
-    $class->SUPER::new(
-      name => 'Tweet',
-      version => '95445312de',
-      author => 'Todd Matthews & Steve Purcell',
-      homepage => 'http://tweet.seaofclouds.com/',
-      css => ['jquery.tweet.css'],
-      javascript => ['jquery.tweet.js', 'jquery.tweet.init.js'],
-      documentation => 'JQTweetContrib',
-      puburl => '%PUBURLPATH%/%SYSTEMWEB%/JQTweetContrib',
-      dependencies => ['metadata'],
-    ),
-    $class
-  );
+    my $this = bless(
+        $class->SUPER::new(
+            name          => 'Tweet',
+            version       => '95445312de',
+            author        => 'Todd Matthews & Steve Purcell',
+            homepage      => 'http://tweet.seaofclouds.com/',
+            css           => ['jquery.tweet.css'],
+            javascript    => [ 'jquery.tweet.js', 'jquery.tweet.init.js' ],
+            documentation => 'JQTweetContrib',
+            puburl        => '%PUBURLPATH%/%SYSTEMWEB%/JQTweetContrib',
+            dependencies  => ['metadata'],
+        ),
+        $class
+    );
 
-  return $this;
+    return $this;
 }
 
 1;
